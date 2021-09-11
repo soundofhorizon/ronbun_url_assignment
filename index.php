2976b6a3a386404bf825c62dce835145ec0fb40b
soundofhorizon <ooshiba0206@gmail.com>
2021-09-12 02:41:55 +0900

    [ADD]some

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
 
            let howmanyunit = document.getElementsByName('howmany');
             if (howmanyunit.item(0).checked){
-                if(single_query.length == 0){
+                if(single_query.length == 1){
                     // unpack
                     single_query.push(package_query[-1][0]);
                     var pick_url = package_query[-1][1];
