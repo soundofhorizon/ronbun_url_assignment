831d29cba0b10dc48f8ae756361880b51d3ca6b9
soundofhorizon <ooshiba0206@gmail.com>
2021-09-12 20:54:37 +0900

    [ADD]some

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
                             $fileName = "実験アクセス用URL記述ファイル-1単位.txt";
                             header('Content-Type: text/plain');
                             header('Content-Disposition: attachment; filename='.$fileName);
+                            echo "到達してます";
                             echo mb_convert_encoding($context, "SJIS", "UTF-8");  //←UTF-8のままで良ければ不要です。
                         }
                     }else{
