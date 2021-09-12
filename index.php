bc45caaa6f878f81a59e645b719bd88a49dd105d
soundofhorizon <ooshiba0206@gmail.com>
2021-09-13 03:43:48 +0900

    [ADD]some

--- index.php
+++ index.php
@@ -3,135 +3,146 @@
     <meta charset="UTF-8">
     <title>URL Assignment System</title>
         <?php
-              if(isset($_POST['howmany'])&&isset($_POST["url_assignment"])){
-                  $conn = pg_connect(getenv("DATABASE_URL"));
-                  // SQLで情報を取得
-                  $package_query = pg_query($conn, 'select Package_query from url_assignment;');
-                  while ($row = pg_fetch_row($package_query)) {
-                      $package_query_result = $row[0];
-                  }
-                  $single_query = pg_query($conn, "select Single_query from url_assignment;");
-                  while ($row = pg_fetch_row($single_query)) {
-                      $single_query_result = $row[0];
-                  }
+            if(isset($_POST['howmany'])&&isset($_POST["url_assignment"])){
+                $conn = pg_connect(getenv("DATABASE_URL"));
+                // SQLで情報を取得
+                $package_query = pg_query($conn, 'select Package_query from url_assignment;');
+                while ($row = pg_fetch_row($package_query)) {
+                    $package_query_result = $row[0];
+                }
+                $single_query = pg_query($conn, "select Single_query from url_assignment;");
+                while ($row = pg_fetch_row($single_query)) {
+                    $single_query_result = $row[0];
+                }
 
-                  //ここで取得したQueryはString表記なのでArrayにする。
-                  $package_query_result = explode("},{",substr($package_query_result, 2, strlen($package_query_result)-4));
-                  //Packageについては、各要素がArrayであってほしいので、更に各要素をexplode.
-                  for($i = 0; $i < count($package_query_result); $i++){
-                      $package_query_result[$i] = explode("," , $package_query_result[$i]);
-                  }
+                //ここで取得したQueryはString表記なのでArrayにする。
+                $package_query_result = explode("},{",substr($package_query_result, 2, strlen($package_query_result)-4));
+                //Packageについては、各要素がArrayであってほしいので、更に各要素をexplode.
+                for($i = 0; $i < count($package_query_result); $i++){
+                    $package_query_result[$i] = explode("," , $package_query_result[$i]);
+                }
 
-                  $single_query_result = explode("," , substr($single_query_result, 1, strlen($single_query_result)-2));
+                $single_query_result = explode("," , substr($single_query_result, 1, strlen($single_query_result)-2));
 
-                  // 単位数の選択によって分岐
-                  $ratio_value = $_POST["howmany"];
-                  switch($ratio_value){
-                      case 1:
-                          // single_query_resultのlengthが1 -> single_queryに何もない。package_query_resultの最後からunpackして追加する。
-                          if(count($single_query_result) == 1){
-                              // package_query_resultに、Queryが残っているかチェック
-                              if(in_array("first", end($package_query_result))){
-                                  $alert = "<script type='text/javascript'>alert('実験の総数が規定を満たした為、現在発行できるURLがありません！申し訳ございません。');</script>";
-                                  echo $alert;
-                                  break;
-                              }else{
-                                  // package_query_resultの最後の要素をunpack
-                                  // 今回発行しないほうをsingleに追加
-                                  array_push($single_query_result, end($package_query_result)[0]);
-                                  $pick_url = end($package_query_result)[1];
-                                  $context = "1単位分の実験参加用のURLです。Google Chromeにて以下のURLからアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/";
-                                  $context .= $pick_url;
-                                  $context .= "-home.html?";
-                                  //学籍番号へメール送信
-                                  if(isset($_POST["url_assignment"])){
-                                        require 'vendor/autoload.php';
+                // 単位数の選択によって分岐
+                $ratio_value = $_POST["howmany"];
+                switch($ratio_value){
+                    case 1:
+                        // single_query_resultのlengthが1 -> single_queryに何もない。package_query_resultの最後からunpackして追加する。
+                        if(count($single_query_result) == 1){
+                            // package_query_resultに、Queryが残っているかチェック
+                            if(in_array("first", end($package_query_result))){
+                                $alert = "<script type='text/javascript'>alert('実験の総数が規定を満たした為、現在発行できるURLがありません！申し訳ございません。');</script>";
+                                echo $alert;
+                                break;
+                            }else{
+                                // package_query_resultの最後の要素をunpack
+                                // 今回発行しないほうをsingleに追加
+                                array_push($single_query_result, end($package_query_result)[0]);
+                                $pick_url = end($package_query_result)[1];
+                                $context = "1単位分の実験参加用のURLです。Google Chromeにて以下のURLからアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/";
+                                $context .= $pick_url;
+                                $context .= "-home.html?";
+                                //学籍番号へメール送信
+                                if(isset($_POST["url_assignment"])){
+                                   require 'vendor/autoload.php';
 
-                                        $email = new \SendGrid\Mail\Mail();
-                                        $email->setFrom("b9p31013@bunkyo.ac.jp", "大柴雅基");
-                                        $email->setSubject("実験アクセス用URL記述メール-1単位");
-                                        $to = $_POST["your_id"];
-                                        $to .= "@bunkyo.ac.jp";
-                                        $email->addTo($to, "参加者様");
-                                        $email->addContent("text/plain", $context);
-                                        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-                                        try {
-                                            $response = $sendgrid->send($email);
-                                        } catch (Exception $e) {
-                                            echo 'Caught exception: '. $e->getMessage() ."\n";
-                                        }
-                                  }
-                                  break;
-                              }
-                          }else{
-                              // '""'の要素は削除
-                              unset($single_query_result[0]);
-                              $index = rand(0, count($single_query_result));
-                              $pick_url = $single_query_result[$index];
-                              unset($single_query_result[$index]);
-                              array_unshift($single_query_result, '""');
-                              $context = "1単位分の実験参加用のURLです。Google Chromeにて以下のURLからアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/";
-                              $context .= $pick_url;
-                              $context .= "-home.html?";
-                              //学籍番号へメール送信
-                              if(isset($_POST["url_assignment"])){
-                                        require 'vendor/autoload.php';
+                                   $email = new \SendGrid\Mail\Mail();
+                                   $email->setFrom("b9p31013@bunkyo.ac.jp", "大柴雅基");
+                                   $email->setSubject("実験アクセス用URL記述メール-1単位");
+                                   $to = $_POST["your_id"];
+                                   $to .= "@bunkyo.ac.jp";
+                                   $email->addTo($to, "参加者様");
+                                   $email->addContent("text/plain", $context);
+                                   $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+                                   try {
+                                       $response = $sendgrid->send($email);
+                                   } catch (Exception $e) {
+                                       echo 'Caught exception: '. $e->getMessage() ."\n";
+                                   }
+                                }
+                                break;
+                            }
+                        }else{
+                            // '""'の要素は削除
+                            unset($single_query_result[0]);
+                            $index = rand(0, count($single_query_result));
+                            $pick_url = $single_query_result[$index];
+                            unset($single_query_result[$index]);
+                            array_unshift($single_query_result, '""');
+                            $context = "1単位分の実験参加用のURLです。Google Chromeにて以下のURLからアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/";
+                            $context .= $pick_url;
+                            $context .= "-home.html?";
+                            //学籍番号へメール送信
+                            if(isset($_POST["url_assignment"])){
+                                require 'vendor/autoload.php';
 
-                                        $email = new \SendGrid\Mail\Mail();
-                                        $email->setFrom("b9p31013@bunkyo.ac.jp", "大柴雅基");
-                                        $email->setSubject("実験アクセス用URL記述メール-1単位");
-                                        $to = $_POST["your_id"];
-                                        $to .= "@bunkyo.ac.jp";
-                                        $email->addTo($to, "参加者様");
-                                        $email->addContent("text/plain", $context);
-                                        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-                                        try {
-                                            $response = $sendgrid->send($email);
-                                        } catch (Exception $e) {
-                                            echo 'Caught exception: '. $e->getMessage() ."\n";
-                                        }
-                              }
-                              break;
-                          }
-                      case 2:
-                               // '""'の要素は削除
-                              unset($package_query_result[0]);
-                              $index = rand(0, count($package_query_result));
-                              $pick_url = $package_query_result[$index];
-                              var_dump($pick_url);
-                              unset($package_query_result[$index]);
-                              array_unshift($package_query_result, array("first", "endpoint"));
-                              $context = "2単位分の実験参加用のURLです。Google Chromeにて以下のURLからアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n\n -1-\n\n https://soundofhorizon.github.io/ronbun-homepage/";
-                              $context .= $pick_url[0];
-                              $context .= "-home.html?\n\n-2-\n\n https://soundofhorizon.github.io/ronbun-homepage/";
-                              $context .= $pack_url[1];
-                              $context .= "-home.html?";
+                                $email = new \SendGrid\Mail\Mail();
+                                $email->setFrom("b9p31013@bunkyo.ac.jp", "大柴雅基");
+                                $email->setSubject("実験アクセス用URL記述メール-1単位");
+                                $to = $_POST["your_id"];
+                                $to .= "@bunkyo.ac.jp";
+                                $email->addTo($to, "参加者様");
+                                $email->addContent("text/plain", $context);
+                                $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+                                try {
+                                    $response = $sendgrid->send($email);
+                                } catch (Exception $e) {
+                                    echo 'Caught exception: '. $e->getMessage() ."\n";
+                                }
+                            }
+                            break;
+                        }
+                    case 2:
+                         // '""'の要素は削除
+                        unset($package_query_result[0]);
+                        $index = rand(0, count($package_query_result));
+                        $pick_url = $package_query_result[$index];
+                        unset($package_query_result[$index]);
+                        array_unshift($package_query_result, array("first", "endpoint"));
+                        $context = "2単位分の実験参加用のURLです。Google Chromeにて以下のURLからアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n\n -1-\n\n https://soundofhorizon.github.io/ronbun-homepage/";
+                        $context .= $pick_url[0];
+                        $context .= "-home.html?\n\n-2-\n\n https://soundofhorizon.github.io/ronbun-homepage/";
+                        $context .= $pick_url[1];
+                        $context .= "-home.html?";
 
-                              //学籍番号へメール送信
-                              if(isset($_POST["url_assignment"])){
-                                        require 'vendor/autoload.php';
+                        //学籍番号へメール送信
+                        if(isset($_POST["url_assignment"])){
+                            require 'vendor/autoload.php';
 
-                                        $email = new \SendGrid\Mail\Mail();
-                                        $email->setFrom("b9p31013@bunkyo.ac.jp", "大柴雅基");
-                                        $email->setSubject("実験アクセス用URL記述メール-2単位");
-                                        $to = $_POST["your_id"];
-                                        $to .= "@bunkyo.ac.jp";
-                                        $email->addTo($to, "参加者様");
-                                        $email->addContent("text/plain", $context);
-                                        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-                                        try {
-                                            $response = $sendgrid->send($email);
-                                        } catch (Exception $e) {
-                                            echo 'Caught exception: '. $e->getMessage() ."\n";
-                                        }
-                              }
-                              break;
-                  }
-              }else{
-                  $alert = "<script type='text/javascript'>alert('単位数を選択してください。');</script>";
-                  echo $alert;
-              }
+                            $email = new \SendGrid\Mail\Mail();
+                            $email->setFrom("b9p31013@bunkyo.ac.jp", "大柴雅基");
+                            $email->setSubject("実験アクセス用URL記述メール-2単位");
+                            $to = $_POST["your_id"];
+                            $to .= "@bunkyo.ac.jp";
+                            $email->addTo($to, "参加者様");
+                            $email->addContent("text/plain", $context);
+                            $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+                            try {
+                                $response = $sendgrid->send($email);
+                            } catch (Exception $e) {
+                                echo 'Caught exception: '. $e->getMessage() ."\n";
+                            }
+                        }
+                        break;
 
+                }
+                // UPDATE SQL
+                $single_update_sql = "";
+                for($i = 0; $i < count($single_query_result); $i++){
+                    $single_query_result[$i] = "'" . $single_query_result[$i] . "'";
+                }
+                $single_sql = "UPDATE url_assignment SET Single_query=ARRAY[" . $single_query_result . "];";
+                var_dump($single_sql);
+                for($i = 0; $i < count($package_query_result); $i++){
+                    $package_query_result[i] = "ARRAY['" . $package_query_result[i][0] . "','" . $package_query_result[i][1] . "']";
+                }
+                $package_sql = "UPDATE url_assignment SET Package_query=ARRAY[" . $package_query_result . "];";
+                var_dump($package_sql);
+            }else{
+                $alert = "<script type='text/javascript'>alert('単位数を選択してください。');</script>";
+                echo $alert;
+            }
         ?>
 </head>
 <body>
@@ -178,40 +189,6 @@
         /*
 
 
-           let howmanyunit = document.getElementsByName('howmany');
-            if (howmanyunit.item(0).checked){
-                if(single_query.length == 1){
-                    // unpack
-                    if(package_query.slice(-1)[0] != "first,endpoint"){
-                        single_query.push(package_query.slice(-1)[0].split(",")[0]);
-                        var pick_url = package_query.slice(-1)[0].split(",")[1];
-                        var context = "1単位分の実験参加用のURLです。以下のURLをコピーし、Google Chromeにてアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/"+pick_url+"-home.html?";
-                        package_query.pop();
-                        downloadAsTextFile("実験アクセス用URL記述ファイル-1単位", context);
-                    }else{
-                        alert("実験の総数が規定を満たした為、現在発行できるURLがありません！申し訳ございません。");
-                    }
-                }else{
-                    var index = Math.floor(Math.random() * single_query.length);
-                    var pick_url = single_query[index];
-                    var context = "1単位分の実験参加用のURLです。以下のURLをコピーし、Google Chromeにてアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。必要な実験環境については事前に配布されているマニュアルを参照してください。\n実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/"+pick_url+"-home.html?";
-                    single_query.splice(index, 1);
-                    downloadAsTextFile("実験アクセス用URL記述ファイル-1単位", context);
-                }
-            }else if(howmanyunit.item(1).checked){
-                if(package_query.slice(-1)[0] != "first,endpoint"){
-                    var index = Math.floor(Math.random() * package_query.length);
-                    var pick_url = package_query[index].split(",");
-                    var context = "2単位分の実験参加用のURLです。以下のURLをそれぞれコピーし、Google Chromeにてアクセスして下さい。\n※発行された分の実験は必ず行うようにしてください。必要な実験環境については事前に配布されているマニュアルを参照してください。\n実験時間は各URL毎30分が想定されています。\n\n----------------------------\n\n 1: https://soundofhorizon.github.io/ronbun-homepage/"+pick_url[0]+"-home.html? \n\n 2: https://soundofhorizon.github.io/ronbun-homepage/"+pick_url[1]+"-home.html?";
-                    package_query.splice(index, 1);
-                    downloadAsTextFile("実験アクセス用URL記述ファイル-2単位", context);
-                }else{
-                   alert("実験の残り数が2単位に満たないため、2単位分のURLが発行できません！1単位で再度お試しください。");
-                }
-            }
-
-            document.getElementById("url_assignment_button").style.display ="none";
-            document.getElementById('result').innerHTML = "<p>URLを発行し、テキストファイルとしてダウンロードしました。\n マニュアルとテキストファイルを参考して実験を進めてください。</p>";
 
             // Update DATABASE Single
             // SQLのtextに対応するためにシングルクォーテーションで要素を全部囲む
